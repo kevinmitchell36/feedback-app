d2c9bc3076f8b7a473646578a1d1abd983f8673e
kevinmitchell36 <kevingm31@gmail.com>
2025-04-10 09:29:59 -0500

    Add real time validation to form, change button to a component, and add selectable radio rating numbers

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,14 +1,22 @@
 import PropTypes from "prop-types" 
 import FeedbackItem from './FeedbackItem'
-
+import { AnimatePresence, motion } from "motion/react"
 function FeedbackList({feedback, handleDelete}) {
   if(!feedback || feedback.lenght === 0) {
     return <p>No feedback yet</p>
   }
   return (
     <div className='feedback-list'>
+      
       {feedback.map( (item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+        <motion.div 
+          key={item.id}
+          initial={{scale: 0}}
+          animate={{scale: 1}}
+          exit={{scale: 0}}
+          >
+          <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+        </motion.div>
       ))}
     </div>
   )
